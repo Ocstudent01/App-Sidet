a98411416f4a9f75fe1dd864801666b628e400a0
orlando <oChC345@gmail.com>
2019-07-21 18:10:21 -0500

    commit inicial

---
+++ app/src/main/java/com/idat/edu/pe/sabores_peruanos/ui/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.idat.edu.pe.sabores_peruanos.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.idat.edu.pe.sabores_peruanos.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
